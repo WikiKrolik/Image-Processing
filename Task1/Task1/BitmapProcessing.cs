097e00fa48e9160ba0fd648a8cbc1e9e4028f15e
Wiktoria <234762@edu.p.lodz.pl>
2022-10-17 10:53:16 +0200

    Project created,function modify brightness

---
+++ Task1/Task1/BitmapProcessing.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing
+{
+    internal class BitmapProcessing
+    {
+
+        public Bitmap LoadPicture(string name)
+        {
+            Bitmap picture = (Bitmap)Bitmap.FromFile(name);
+            return picture;
+        }
+
+        public void SavePicture(Bitmap picture)
+        {
+            picture.Save("C://test//Light.png", ImageFormat.Png);
+        }
+
+        public Bitmap ModifyBrightness(Bitmap picture, int brightness)
+        {
+            for (int x = 0; x < picture.Width; x++)
+            {
+                for (int y = 0; y < picture.Height; y++)
+                {
+                    Color pixelColor = picture.GetPixel(x, y);
+
+                    int r = brightness + pixelColor.R;
+                    int g = brightness + pixelColor.G;
+                    int b = brightness + pixelColor.B;
+
+                    r = Math.Clamp(r, 0, 255);
+                    g = Math.Clamp(g, 0, 255);
+                    b = Math.Clamp(b, 0, 255);
+
+                    picture.SetPixel(x, y, Color.FromArgb(pixelColor.A, r, g, b));
+                }
+
+            }
+            return picture;
+        }
+
+        /*public Bitmap ModifyContrast(Bitmap picture, int contrast)
+        {
+            for (int x = 0; x < picture.Width; x++)
+            {
+                for (int y = 0; y < picture.Height; y++)
+                {
+                    Color pixelColor = picture.GetPixel(x, y);
+                    double v =  Math.Pow((100.0 + contrast) / 100.0, 2);
+
+                    int r = (int)((pixelColor.R / 255 - 0.5) * v + 0.5) * 255;
+                    int g = (int)((pixelColor.G / 255 - 0.5) * v + 0.5) * 255;
+                    int b = (int)((pixelColor.B / 255 - 0.5) * v + 0.5) * 255;
+
+                    r = Math.Clamp(r, 0, 255);
+                    g = Math.Clamp(g, 0, 255);
+                    b = Math.Clamp(b, 0, 255);
+
+                    picture.SetPixel(x, y, Color.FromArgb(pixelColor.A, r, g, b));
+                }
+
+            }
+            return picture;
+        }*/
+    }
+}
+
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new BitmapProcessing();
+            string name = "C://test//lenac_small.bmp";
+            Bitmap picture = p.LoadPicture(name);
+            picture = p.ModifyBrightness(picture, 50);
+            p.SavePicture(picture);
+
+        }
+
+    }
+}
